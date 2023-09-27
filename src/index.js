2b59a99f379720c5c148810f79853ec2d8c7d8e1
BrandonSGC <brangmz20@gmail.com>
2023-09-27 17:27:22 -0600

    Server configuration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import app from './app.js';
+import {sequelize} from './database/connection.js';
+import './models/Roles.js';
+import './models/Usuario.js';
+
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT);
+async function main() {
+  try {
+    // Testing the connection.
+    // await sequelize.authenticate();
+    // console.log('Connection has been established successfully.');
 
-console.log(`Server running at http://localhost:${PORT}`);
+    // Handle the DB.
+    await sequelize.sync({force: true}/**/);
+    app.listen(PORT);
+    console.log(`Server running at http://localhost:${PORT}`);
+
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+main();
