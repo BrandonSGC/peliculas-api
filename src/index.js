842b54fe03ac6915eba0f23acad4e00ad0c035a8
BrandonSGC <brangmz20@gmail.com>
2023-09-27 19:09:30 -0600

    Test api file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import {sequelize} from './database/connection.js';
 import './models/Roles.js';
 import './models/Usuario.js';
 
-
 const PORT = process.env.PORT || 3000;
 
 async function main() {
@@ -12,8 +11,10 @@ async function main() {
     // await sequelize.authenticate();
     // console.log('Connection has been established successfully.');
 
-    // Handle the DB.
-    await sequelize.sync({force: true}/**/);
+    // Handle DB.
+    await sequelize.sync(/*{force: true}*/);
+
+    // Server.
     app.listen(PORT);
     console.log(`Server running at http://localhost:${PORT}`);
 
