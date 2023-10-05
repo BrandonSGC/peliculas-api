1a57b7d40795ef87c28d7ea05bce3639b70d151f
santiago <santiago123araya@gmail.com>
2023-10-04 18:59:44 -0600

    funcionalidad involucrados

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import userRoutes from './routes/users.routes.js';
 import calificadoresRoutes from "./routes/calificadores.routes.js";
 import expertosRoutes from "./routes/expertos.routes.js"; 
 import moviesRoutes from './routes/movies.routes.js'
+import involucradoRoutes from './routes/involucrado.routes.js';
+
 
 const app = express();
 
@@ -13,5 +15,7 @@ app.use(userRoutes);
 app.use(calificadoresRoutes);
 app.use(expertosRoutes); 
 app.use(moviesRoutes);
+app.use(involucradoRoutes);
+
 
 export default app;
---
+++ src/models/Involucrado.js
@@ -0,0 +1,39 @@
+import { sequelize } from '../database/connection.js';
+import { DataTypes } from 'sequelize';
+
+const Involucrado = sequelize.define(
+  'Involucrado',
+  {
+    involucradoID: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    nombre: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+    },
+    paginaWeb: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    facebook: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    instagram: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+    twitter: {
+      type: DataTypes.STRING(255),
+      allowNull: true,
+    },
+  },
+  {
+    timestamps: false,
+    tableName: 'Involucrado',
+  }
+);
+
+export default Involucrado;
