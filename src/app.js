96bf299b8ae42b59d495549cfbf52b1886003120
BrandonSGC <brangmz20@gmail.com>
2023-10-31 13:17:42 -0600

    Implementing cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import cors from 'cors';
+
 import userRoutes from './routes/users.routes.js';
 import calificadoresRoutes from "./routes/calificadores.routes.js";
 import expertosRoutes from "./routes/expertos.routes.js"; 
@@ -6,10 +8,10 @@ import moviesRoutes from './routes/movies.routes.js'
 import involucradoRoutes from './routes/involucrado.routes.js';
 import tipoInvolucrados from './routes/roles.routes.js'
 
-
 const app = express();
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 
 app.use(userRoutes);
